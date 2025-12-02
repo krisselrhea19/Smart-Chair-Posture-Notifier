fe45da26d57006aae606edbbae77a19a79b92c55
KeeShenoy <keerthanashenoy05@gmail.com>
2025-12-02 16:48:12 +0530

    Added test code for FSRs

---
+++ ESP32-code/FSR_Test_Code.ino
@@ -0,0 +1,37 @@
+
+
+
+const int FSR1_PIN = 34;   // ADC pin - FSR1
+const int FSR2_PIN = 35;   // ADC pin - FSR2
+
+// --- Temp Threshold Value ---
+int THRESHOLD = 500;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("FSR Posture Test Started...");
+}
+
+void loop() {
+  int fsr1 = analogRead(FSR1_PIN);
+  int fsr2 = analogRead(FSR2_PIN);
+
+  
+  Serial.print("FSR1: ");
+  Serial.print(fsr1);
+  Serial.print(" | FSR2: ");
+  Serial.println(fsr2);
+
+  //  threshold logic for time being
+  if (fsr1 > THRESHOLD && fsr2 > THRESHOLD) {
+    Serial.println("Posture: GOOD ");
+  } 
+  else {
+    Serial.println("Posture: BAD ");
+  }
+
+  Serial.println("-------------------------");
+  delay(500);
+}
